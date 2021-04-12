64f8fb6ceeff2b755efe9e9115739897ca48cf2b
harthur-jam <dzhemakulov.art@gmail.com>
2021-04-12 17:09:18 +0300

    Edited

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-apt install python3-pip python3-virtualenv -y
+# apt install python3-pip python3-virtualenv -y
 virtualenv venv --python=python3
 source venv/bin/activate
 pip3 install -r requirements.txt
