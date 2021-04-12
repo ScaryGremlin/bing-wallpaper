3ef74f79d5033d74ffaa0ec5461725cb691c5433
harthur-jam <dzhemakulov.art@gmail.com>
2021-04-12 17:45:29 +0300

    Edited

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-virtualenv venv --python=python3
-source venv/bin/activate
-pip3 install -r requirements.txt
-deactivate
-PATH_BING_WALLPAPER=$(pwd)
-(crontab -l 2>/dev/null; echo "0 */1 * * * $PATH_BING_WALLPAPER/venv/bin/python3 $PATH_BING_WALLPAPER/app/bing-wallpaper.py") | crontab -
